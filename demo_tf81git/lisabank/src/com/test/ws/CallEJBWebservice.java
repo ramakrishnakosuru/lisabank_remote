6be2ac23737bc7671df136c847b46d19884de02a
RK108807 <RK108807@INHYIZLP06681.TechMahindra.com>
2016-02-15 13:56:55 +0530

    initial commit rk1

---
+++ demo_tf81git/lisabank/src/com/test/ws/CallEJBWebservice.java
@@ -0,0 +1,27 @@
+package com.test.ws;
+
+import java.rmi.RemoteException;
+
+import com.itko.examples.ejb3.EJB3UserControlBean;
+import com.itko.examples.ejb3.EJB3UserControlBeanProxy;
+import com.itko.examples.ejb3.User;
+
+public class CallEJBWebservice {
+
+	public static void main(String args[])
+	{
+		
+		System.out.println("Ramakrishna");
+		EJB3UserControlBeanProxy prox=new EJB3UserControlBeanProxy();
+		EJB3UserControlBean ejb3bean= prox.getEJB3UserControlBean();
+	User users[] = null;
+	try {
+		users = ejb3bean.listUsers();
+	} catch (RemoteException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	System.out.println("length is :"+ users.length);
+		}
+	
+}
